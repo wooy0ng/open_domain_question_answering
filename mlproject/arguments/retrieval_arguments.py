0ac51c8effef97555fb22e66b45eb4c8e22e135c
wooy0ng <yygg9800@naver.com>
2023-05-03 16:47:55 +0900

    [feat]: refactoring

---
+++ mlproject/arguments/retrieval_arguments.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class RetrievalArguments:
+    dataset_name: str = field(
+        default='../data/train_dataset',
+        metadata={
+            'help': ""
+        }
+    )
+    model_name_or_path: str = field(
+        default='bert-base-multilingual-cased',
+        metadata={
+            'help': ""
+        }
+    )
+    data_path: str = field(
+        default='../data',
+        metadata={
+            'help': ""
+        }
+    )
+    context_path: str = field(
+        default='../data/wikipedia_documents.json',
+        metadata={
+            'help': ""
+        }
+    )
+    use_faiss: bool = field(
+        default=False,
+        metadata={
+            'help': ""
+        }
+    )
---
+++ mlproject/train_retriever.py
@@ -0,0 +1,56 @@
+import argparse
+import time
+import numpy as np
+import pandas as pd
+
+from utils import timer
+from arguments.retrieval_arguments import RetrievalArguments
+from transformers import HfArgumentParser, AutoTokenizer
+from datasets import load_from_disk, concatenate_datasets
+from retriever.sparse_retrieval import SparseRetrieval
+
+
+if __name__ == '__main__':
+    parser = HfArgumentParser(RetrievalArguments)
+    retrieval_args, = parser.parse_args_into_dataclasses()
+    
+    # Test sparse
+    org_dataset = load_from_disk(retrieval_args.dataset_name)
+    full_ds = concatenate_datasets([
+        org_dataset['train'].flatten_indices(), org_dataset['validation'].flatten_indices()
+    ])
+    
+    print("query datasets")
+    print(full_ds)
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        retrieval_args.model_name_or_path,
+        use_fast=False,
+    )
+    retriever = SparseRetrieval(
+        tokenize_fn=tokenizer.tokenize,
+        data_path=retrieval_args.data_path,
+        context_path=retrieval_args.context_path
+    )
+    
+    query = "대통령을 포함한 미국의 행정부 견제권을 갖는 국가 기관은?"
+    
+    if retrieval_args.use_faiss:
+        # test single query
+        pass
+
+    else:
+        retriever.get_sparse_embedding()
+        with timer("bulk query by exhaustive search"):
+            df = retriever.retrieve(full_ds)
+            df['correct'] = df['original_context'] == df['context']
+            
+            print(
+                "correct retrieval result by exhausive search",
+                df['correct'].sum() / len(df)
+            )
+
+        with timer("single query by exhausive search"):
+            scores, context = retriever.retrieve(query)
+            
+    pass
